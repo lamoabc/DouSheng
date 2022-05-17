4a67f45e38005d5d07adf512bf54937304c95b47
Lamo <704915994@qq.com>
2022-05-17 20:19:10 +0800

    update

--- Code/v1.0/Controller/loginController.go
+++ Code/v1.0/Controller/loginController.go
@@ -2,9 +2,6 @@ package controller
 
 import (
 	"Code/v1.0/models"
-	"net/http"
-
-	"sync/atomic"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,15 +9,6 @@ import (
 // usersLoginInfo use map to store user info, and key is username+password for demo
 // user data will be cleared every time the server starts
 // test data: username=zhanglei, password=douyin
-var usersLoginInfo = map[string]models.User{
-	"zhangleidouyin": {
-		Id:            1,
-		Name:          "zhanglei",
-		FollowCount:   10,
-		FollowerCount: 5,
-		IsFollow:      true,
-	},
-}
 
 var userIdSequence = int64(1)
 
@@ -35,61 +23,15 @@ type UserResponse struct {
 	User models.User `json:"user"`
 }
 
-func Register(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
-
-	token := username + password
-
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: models.Response{StatusCode: 1, StatusMsg: "User already exist"},
-		})
-	} else {
-		atomic.AddInt64(&userIdSequence, 1)
-		newUser := models.User{
-			Id:   userIdSequence,
-			Name: username,
-		}
-		usersLoginInfo[token] = newUser
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: models.Response{StatusCode: 0},
-			UserId:   userIdSequence,
-			Token:    username + password,
-		})
-	}
-}
-
 func Login(c *gin.Context) {
+
 	username := c.Query("username")
 	password := c.Query("password")
 
 	token := username + password
 
-	if user, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: models.Response{StatusCode: 0},
-			UserId:   user.Id,
-			Token:    token,
-		})
-	} else {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: models.Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
-	}
 }
 
 func UserInfo(c *gin.Context) {
-	token := c.Query("token")
 
-	if user, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: models.Response{StatusCode: 0},
-			User:     user,
-		})
-	} else {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: models.Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
-	}
 }
--- Code/v1.0/models/user.go
+++ Code/v1.0/models/user.go
@@ -6,9 +6,19 @@ type Response struct {
 }
 
 type User struct {
-	Id            int64  `json:"id,omitempty"`
-	Name          string `json:"name,omitempty"`
-	FollowCount   int64  `json:"follow_count,omitempty"`
-	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	Id   int64  `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	// FollowCount   int64  `json:"follow_count,omitempty"`
+	// FollowerCount int64  `json:"follower_count,omitempty"`
+	// IsFollow      bool   `json:"is_follow,omitempty"`
+}
+type UserLoginResponse struct {
+	Response
+	UserId int64  `json:"user_id,omitempty"`
+	Token  string `json:"token"`
+}
+
+type UserResponse struct {
+	Response
+	User User `json:"user"`
 }
