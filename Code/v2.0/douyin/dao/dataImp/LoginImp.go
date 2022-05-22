31def405a6b3dacb7cb1c61074bce49f81642741
李永嘉 <10999723+zhi-qieryi@user.noreply.gitee.com>
2022-05-22 17:15:26 +0000

    V2.0 update

---
+++ Code/v2.0/douyin/dao/dataImp/LoginImp.go
@@ -0,0 +1,19 @@
+package dataImp
+
+import (
+	"douyin/dao"
+	"douyin/module"
+	"gorm.io/gorm"
+)
+
+func Login(username *string, password *string, data *module.UserTable) string {
+	// Judge whether the username and password is it false
+	err := dao.Db.Where("user_name = ? AND account_password = ? ", username, password).First(&data).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return "The username or password is incorrect"
+		}
+		return err.Error()
+	}
+	return ""
+}
