18f6007b30e8ea6daff57e41b98f7e1c35cb3203
李永嘉 <10999723+zhi-qieryi@user.noreply.gitee.com>
2022-05-26 19:51:33 +0000

    update Code/v2.0/douyin/module/videoEntity.go.

--- Code/v2.0/douyin/module/videoEntity.go
+++ Code/v2.0/douyin/module/videoEntity.go
@@ -1,14 +1,14 @@
 package module
 
 type Video struct {
-	Id            int64  `json:"id,omitempty"`
+	Id            int64  `json:"id"`
 	Author        User   `json:"author"`
-	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
-	CoverUrl      string `json:"cover_url,omitempty"`
-	FavoriteCount int64  `json:"favorite_count,omitempty"`
-	CommentCount  int64  `json:"comment_count,omitempty"`
-	IsFavorite    bool   `json:"is_favorite,omitempty"`
+	PlayUrl       string `json:"play_url"`
+	CoverUrl      string `json:"cover_url"`
+	FavoriteCount int64  `json:"favorite_count"`
 	VideoTitle    string `json:"title"`
+	CommentCount  int64  `json:"comment_count"`
+	IsFavorite    bool   `json:"is_favorite"`
 }
 type VideoTable struct {
 	VideoId    int64  `gorm:"column:video_id"`
@@ -18,5 +18,10 @@ type VideoTable struct {
 	VideoTitle string `gorm:"column:video_title"`
 	FavCount   int64  `gorm:"column:favourite_count"`
 	ComCount   int64  `gorm:"column:comment_count"`
-	UploadDate string `gorm:"column:upload_date"`
+	UploadDate int64  `gorm:"column:upload_date"`
+}
+
+func (u VideoTable) TableName() string {
+	// 绑定 Mysql 表名
+	return "video"
 }
