0061c036353e0d36aa0c01774fcb7f8fd8d406ad
李永嘉 <10999723+zhi-qieryi@user.noreply.gitee.com>
2022-05-26 19:50:41 +0000

    update Code/v2.0/douyin/service/userService.go.

--- Code/v2.0/douyin/service/userService.go
+++ Code/v2.0/douyin/service/userService.go
@@ -5,6 +5,7 @@ import (
 	"douyin/module"
 	"douyin/module/jsonModule/response"
 	"douyin/tools"
+    "douyin/dao/feedImp"
 )
 
 func login(u *module.User) {
@@ -36,4 +37,80 @@ func Register(username string, password string, response *response.Register) {
 	response.StatusCode = 0
 	response.StatusMsg = "successful"
 	return
+}
+func Feed(latestTime int64, token string, response *response.Feed) {
+	//用户登录状态
+	//解析token拿到用户信息
+	user, err := tools.AnalyseToken(token)
+	if err != nil {
+		response.StatusCode = -1
+		response.StatusMsg = "Token Encryption failed"
+		return
+	}
+	//token无误
+	//声明视频流需要到数据库里拿的module,去数据库拿值
+	var data []module.VideoWithAuthor
+	var message string
+	if latestTime > 0 {
+		//限制时间戳
+		message = feedImp.Feed2(latestTime, &data)
+	} else {
+		//没有限制时间戳
+		message = feedImp.Feed1(&data)
+	}
+	if message != "" || len(data) < 1 {
+		//拿data过程有异常
+		response.StatusCode = -1
+		response.StatusMsg = message
+		return
+	}
+	//data无误拿到
+	//根据userid查用户对data里的视频是否喜欢
+	var isFav []bool
+	for i := 0; i < len(data); i++ {
+		isFav[i], message = feedImp.Feed3(user.UserId, data[i].VideoId)
+		if message != "" {
+			break
+		}
+	}
+	if message != "" {
+		//查是否喜欢过程中有异常
+		response.StatusCode = -1
+		response.StatusMsg = message
+		return
+	}
+	//根据userid查用户对data里视频的作者是否关注
+	var isFol []bool
+	for i := 0; i < len(data); i++ {
+		isFol[i], message = feedImp.Feed4(data[i].AuthorId, user.UserId)
+		if message != "" {
+			break
+		}
+	}
+	if message != "" {
+		//查是否喜欢过程中有异常
+		response.StatusCode = -1
+		response.StatusMsg = message
+		return
+	}
+	//data,isFav,isFol无误拿到,装填response
+	var VideoList = [5]module.Video{}
+	for i := 0; i < len(data); i++ {
+		VideoList[i].Id = data[i].VideoId
+		VideoList[i].Author.Id = data[i].UserId
+		VideoList[i].Author.Name = data[i].Username
+		VideoList[i].Author.IsFollow = isFol[i]
+		VideoList[i].Author.FollowCount = data[i].FollowCount
+		VideoList[i].Author.FollowerCount = data[i].FollowerCount
+		VideoList[i].CommentCount = data[i].ComCount
+		VideoList[i].FavoriteCount = data[i].FavCount
+		VideoList[i].CoverUrl = data[i].CoverUrl
+		VideoList[i].IsFavorite = isFav[i]
+		VideoList[i].PlayUrl = data[i].PlayUrl
+		VideoList[i].VideoTitle = data[i].VideoTitle
+	}
+	response.StatusCode = 0
+	response.StatusMsg = "successful"
+	response.NextTime = data[4].UploadDate
+	response.List = VideoList
 }
