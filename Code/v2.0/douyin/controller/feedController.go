3133937cfe48ea058aff1d194f95e428665e122e
李永嘉 <10999723+zhi-qieryi@user.noreply.gitee.com>
2022-05-26 19:45:35 +0000

    update Code/v2.0/douyin/controller/feedController.go.

--- Code/v2.0/douyin/controller/feedController.go
+++ Code/v2.0/douyin/controller/feedController.go
@@ -1,19 +1,31 @@
 package controller
 
 import (
+	"douyin/module/jsonModule/response"
+	"douyin/service/userService"
+	"douyin/service/visitorService"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 func Feed(c *gin.Context) {
-	//token := c.Query("token")
-	//latestTime := c.Query("latest_time")
-	//if token == "" {
-	//	var VideoList []module.Video
-	//	dao.Db.Find(&VideoList).Limit(5).Where("upload_date")
-	//	c.JSON(http.StatusOK, FeedResponse{
-	//		Response:  module.Response{StatusCode: 0, StatusMsg: "填充视频流成功"},
-	//		VideoList:
-	//		NextTime:  time.Now().Unix(),
-	//	})
-	//}
+	token := c.Query("token")
+	Time := c.Query("latest_time")
+	latestTime, err := strconv.ParseInt(Time, 10, 64)
+	if err != nil {
+		latestTime = 0
+	}
+	var response response.Feed
+	if token == "" {
+		//游客身份
+		//调用游客Feed流服务装填response
+		visitorService.Feed(latestTime, &response)
+		c.JSON(http.StatusOK, response)
+	} else {
+		//用户身份
+		//调用用户Feed流服务装填response
+		userService.Feed(latestTime, token, &response)
+		c.JSON(http.StatusOK, response)
+	}
 }
