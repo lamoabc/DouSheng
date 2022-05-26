92fe31f71e03b2321553439c3d5dfbfd1a6b307e
李永嘉 <10999723+zhi-qieryi@user.noreply.gitee.com>
2022-05-26 19:47:19 +0000

    V3.0 update

---
+++ Code/v2.0/douyin/dao/feedImp/FeedImp.go
@@ -0,0 +1,58 @@
+package feedImp
+
+import (
+	"douyin/dao"
+	"douyin/module"
+	"gorm.io/gorm"
+)
+
+func Feed3(userId int64, videoId int64) (exist bool, message string) {
+	//根据用户id和视频id查询用户是否点赞过该视频
+	temp := module.FavTable{}
+	temp.FavVideoId = 10
+	temp.FavUserId = 10
+	err := dao.Db.Where("fav_user_id=? and fav_video_id=?", userId, videoId).Take(&temp).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, "select exception"
+	}
+	if temp.FavUserId == userId && temp.FavVideoId == videoId {
+		return true, ""
+	}
+	return false, ""
+}
+func Feed4(userId int64, authorId int64) (exist bool, message string) {
+	//根据用户id和作者id查询用户是否关注了该作者
+	temp := module.FollowTable{}
+	err := dao.Db.Where("follow_id=? and follower_id=?", authorId, userId).Take(&temp).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, "select exception"
+	}
+	if temp.FollowId == authorId && temp.FollowerId == userId {
+		return true, ""
+	}
+	return false, ""
+}
+func Feed2(latestTime int64, data *[]module.VideoWithAuthor) (message string) {
+	//有限制时间戳,返回视频表里比给定时间戳更小即更早的五个视频
+	//装填data,如果有错误信息,通过写入message直接返回
+	err := dao.Db.Where("upload_date<?", latestTime).Order("upload_date desc").Limit(5).Offset(0).Find(&data).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return "Cant find any result"
+		}
+		return err.Error()
+	}
+	return ""
+}
+func Feed1(data *[]module.VideoWithAuthor) (message string) {
+	//没有限制时间戳,返回视频表里时间最早的五个视频
+	//装填data,如果有错误信息,通过写入message直接返回
+	err := dao.Db.Order("upload_date desc").Limit(5).Offset(0).Find(&data).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return "Cant find any result"
+		}
+		return err.Error()
+	}
+	return ""
+}
