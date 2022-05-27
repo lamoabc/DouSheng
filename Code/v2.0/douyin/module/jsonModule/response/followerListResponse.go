63dc28d947997f2d745d88457aa4d5e0aaad8883
zhaozhitao <1376304424@qq.com>
2022-05-27 22:51:59 +0800

    粉丝列表接口

--- Code/v2.0/douyin/module/jsonModule/response/followerListResponse.go
+++ Code/v2.0/douyin/module/jsonModule/response/followerListResponse.go
@@ -1,1 +1,8 @@
 package response
+
+import "douyin/module"
+
+type FollowerList struct {
+	module.Response
+	UserList []module.User `json:"user_list"`
+}
--- Code/v2.0/douyin/service/relationService/relationService.go
+++ Code/v2.0/douyin/service/relationService/relationService.go
@@ -46,3 +46,43 @@ func FollowList(token string, userId int64, response *response.FollowList) {
 	response.StatusMsg = "successful"
 	response.UserList = userListResp
 }
+
+func FollowerList(token string, userId int64, response *response.FollowList) {
+	// 根据userId查询出该用户的粉丝列表
+	var userList []module.UserTable
+	if err := relationImp.QueryFollwerListUserById(userId, &userList); err != nil {
+		response.StatusCode = -1
+		response.StatusMsg = err.Error()
+		return
+	}
+
+	// 查询当前用户是否关注了列表中的用户
+	isFolList := make([]bool, len(userList))
+	if token != "" {
+		userClaims, err := tools.AnalyseToken(token)
+		if err != nil {
+			response.StatusCode = -1
+			response.StatusMsg = err.Error()
+			return
+		}
+		isFolList, err = relationImp.IsFollwer(userClaims.UserId, userList)
+		if err != nil {
+			response.StatusCode = -1
+			response.StatusMsg = err.Error()
+			return
+		}
+	}
+	userListResp := make([]module.User, len(userList))
+	for i := 0; i < len(userList); i++ {
+		userListResp[i].Id = userList[i].UserId
+		userListResp[i].Name = userList[i].Username
+		userListResp[i].FollowCount = userList[i].FollowCount
+		userListResp[i].FollowerCount = userList[i].FollowerCount
+		userListResp[i].IsFollow = isFolList[i]
+	}
+
+	response.StatusCode = 0
+	response.StatusMsg = "successful"
+	response.UserList = userListResp
+
+}
