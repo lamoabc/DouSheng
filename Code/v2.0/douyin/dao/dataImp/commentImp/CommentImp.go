c13dfb671b1062a81e87bcc68368e8084f7196ff
Lamo <704915994@qq.com>
2022-05-28 10:25:29 +0800

    update CommentAction

--- Code/v2.0/douyin/dao/dataImp/commentImp/CommentImp.go
+++ Code/v2.0/douyin/dao/dataImp/commentImp/CommentImp.go
@@ -11,16 +11,13 @@ func QueryUserId(userId int64, usertable *module.UserTable) (err error) {
 	return
 }
 
-func InsertCommentMsg(vedioId int64, userId int64, content string, createDate string) error {
-	err := dao.Db.Select("com_video_id", "com_user_id", "content", "create_date").Create(module.CommentTable{ComVideoId: vedioId, ComUserId: userId, Content: content, CreateDate: createDate}).Error
-	return err
+func InsertCommentMsg(vedioId int64, userId int64, content string, createDate string) (err error) {
+	err = dao.Db.Select("com_video_id", "com_user_id", "content", "create_date").Create(module.CommentTable{ComVideoId: vedioId, ComUserId: userId, Content: content, CreateDate: createDate}).Error
+	return
 }
 
 func QueryCommentMsgRes(vedioId int64, userId int64, createDate string, commentTable *module.CommentTable) (err error) {
 	err = dao.Db.Where(module.CommentTable{ComVideoId: vedioId, ComUserId: userId, CreateDate: createDate}).Find(&commentTable).Error
-	if err != nil {
-		return err
-	}
 	return
 }
 
