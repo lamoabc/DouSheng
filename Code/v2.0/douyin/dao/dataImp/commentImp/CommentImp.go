fac27dda2638896c2d6df46775fd0daa699a1ae7
Lamo <704915994@qq.com>
2022-05-28 10:27:14 +0800

    update CommentAction

--- Code/v2.0/douyin/dao/dataImp/commentImp/CommentImp.go
+++ Code/v2.0/douyin/dao/dataImp/commentImp/CommentImp.go
@@ -11,16 +11,19 @@ func QueryUserId(userId int64, usertable *module.UserTable) (err error) {
 	return
 }
 
+// make commentMsg insert database
 func InsertCommentMsg(vedioId int64, userId int64, content string, createDate string) (err error) {
 	err = dao.Db.Select("com_video_id", "com_user_id", "content", "create_date").Create(module.CommentTable{ComVideoId: vedioId, ComUserId: userId, Content: content, CreateDate: createDate}).Error
 	return
 }
 
+// Query commentMsg
 func QueryCommentMsgRes(vedioId int64, userId int64, createDate string, commentTable *module.CommentTable) (err error) {
 	err = dao.Db.Where(module.CommentTable{ComVideoId: vedioId, ComUserId: userId, CreateDate: createDate}).Find(&commentTable).Error
 	return
 }
 
+// Delete comment
 func DeleteCommentImp(commentId int64, CommentTable *module.CommentTable) (err error) {
 
 	err = dao.Db.Where("comment_id", commentId).Delete(&CommentTable).Error
